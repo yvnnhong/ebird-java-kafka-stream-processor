c3cfa911ced9621d2774b0c3b7d1862b6e3dde9f
Yvonne Hong <yvonnehong2003@gmail.com>
2025-06-27 22:31:24 -0700

    note to self: weigh recent patterns for s7 more heavily for the streaming system

--- scripts/analyze_seasonal_patterns.py
+++ scripts/analyze_seasonal_patterns.py
@@ -1,5 +1,9 @@
 # scripts/analyze_seasonal_patterns.py
-#note for tomorrow: double check the output 
+#note for tomorrow: double check the output (just did this nvm)
+#note for tomorrow: make sure that the streaming logic will weight the 
+#s7 more heavily as the years go by
+# streaming system should weight recent patterns 
+# more heavily (s7 baselin becomes more reliable/frequent)
 import pandas as pd
 
 df = pd.read_csv('../data/results_csv/mourning_dove_baseline_complete.csv')
